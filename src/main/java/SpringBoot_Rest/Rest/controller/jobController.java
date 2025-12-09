d0526077f67ca187cc4029ca3ff420cdc481c4fa
Himanshu <himanshu.k.upadhyay05@gmail.com>
2025-12-09 22:24:32 +0530

    basics

---
+++ src/main/java/SpringBoot_Rest/Rest/controller/jobController.java
@@ -0,0 +1,30 @@
+package SpringBoot_Rest.Rest.controller;
+
+import SpringBoot_Rest.Rest.model.JobPost;
+import SpringBoot_Rest.Rest.service.jobService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+@RestController
+@CrossOrigin("http://localhost:3000")
+public class jobController {
+
+    @Autowired
+    private jobService js ;
+
+    @GetMapping("jobposts")
+    public List<JobPost> getJobs() {
+        return js.getJobPost();
+    }
+
+    @GetMapping("jobpost/{id}")
+    public JobPost getJob(@PathVariable("id") int id){
+        return js.getjob(id);
+    }
+
+    @PostMapping("jobposts")
+    public void addJob(@RequestBody JobPost j) {
+        js.addJob(j);
+    }
+}
---
+++ src/main/java/SpringBoot_Rest/Rest/service/jobService.java
@@ -0,0 +1,26 @@
+package SpringBoot_Rest.Rest.service;
+
+import SpringBoot_Rest.Rest.model.JobPost;
+import SpringBoot_Rest.Rest.repo.jobRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+@Service
+public class jobService {
+
+    @Autowired
+    private jobRepo repo;
+
+    public void addJob(JobPost j){
+        repo.addJob(j);
+    }
+
+    public List<JobPost> getJobPost(){
+        return repo.getJobPost();
+    }
+
+    public JobPost getjob(int id) {
+        return repo.getJob(id);
+    }
+}
